931837c5df49130084b159ecf907ead00a955f14
ilseku <54395297+ilseku@users.noreply.github.com>
2019-09-04 14:00:04 +0200

    Update project1.cpp

--- project1.cpp
+++ project1.cpp
@@ -3,6 +3,7 @@
 #include <cstdlib>
 #include <fstream>
 #include <iomanip>
+#include "time.h"
 
 std::ofstream ofile;
 
@@ -14,6 +15,8 @@ double u(double x){
   return 1 - (1- exp(-10))*x - exp(-10*x);
 }
 int main(int argc, char *argv[]){
+  clock_t start, finish;
+  start = clock();
   //initialize vectors for a, b, c, b_tilde, x and v
   int n = atof(argv[1]);//number of points we calculate on
   int N = n+2;//actual number of elements (includes x_0 and x_n+1)
@@ -22,19 +25,19 @@ int main(int argc, char *argv[]){
   double *b_tilde = new double[N];
   double *c = new double[N];
   double *a = new double[N];
-  double *x = new double[N]; // x has n+2 long
+  double *x = new double[N];
   double h = 1.0/(n+1);
-  x[0] = 0, x[1] = h, x[n+1] = 1;
+  x[0] = 0, x[1] = h, x[n+1] = 1; //initialising first elements in the vectors
   a[1] = -1., b[1] = 2., c[1] = -1;
-  double hh = h*h;
+  double hh = h*h; //do this outside loop to minimize flops in loop
 
 //doing a loop for finding the new values for vectors
-  std::cout << "Starting step 1, overwrites b and b_tilde\n" << std::endl;
+  std::cout << "Starting step 1, overwrites b and b_tilde" << std::endl;
   for(int i = 2; i < n+1; i++){
     a[i] = -1, c[i] =-1, b[i] = 2; //initialising vecotors a, b, c
     x[i] = i*h; // making vector x
     b_tilde[i] = hh*f(x[i]); // making vector b_tilde
-    b[i] -= c[i-1]*a[i-1]/b[i-1]; 
+    b[i] -= c[i-1]*a[i-1]/b[i-1];
     b_tilde[i] -= b_tilde[i-1]*a[i-1]/b[i-1];
 
   }
@@ -52,12 +55,10 @@ int main(int argc, char *argv[]){
 
   using namespace std;
   double diff;
-  double avg_error = 0.; 
-  double solution;
+  double avg_error = 0.;
   for (int i=1; i<=n; i++){
-    solution = 1 - (1-exp(-10))*x[i] - exp(-10*x[i]);
-    diff = abs(v[i] - solution);
-    avg_error += diff/solution;
+    diff = abs(v[i] - u(x[i]));
+    avg_error += log10(diff/u(x[i])); //logarithm of relative error
     }
   avg_error /= n;
   cout << "Average relative error = " << avg_error << endl;
@@ -68,5 +69,7 @@ int main(int argc, char *argv[]){
     ofile << setw(5) << setprecision(6) <<" x= "<< x[i] << "  u= " << u(x[i])<< " v= " << v[i] << endl;
   }
   ofile.close();
+  finish = clock();
+  cout <<"Time elapsed =  " << ( (double)(finish - start)/ CLOCKS_PER_SEC ) <<" seconds" <<endl;
   return 0;
 }
