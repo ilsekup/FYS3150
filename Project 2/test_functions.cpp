4663270f6836ae3f8550b77bbca961b6ecf9eb5f
ilseku <54395297+ilseku@users.noreply.github.com>
2019-09-23 14:26:05 +0200

    Update test_functions.cpp

--- Project 2/test_functions.cpp
+++ Project 2/test_functions.cpp
@@ -1,6 +1,6 @@
 #include "catch.hpp"
 #include "Project2.h"
-
+/*
 TEST_CASE("Testing Maximum Values"){
   int N = 3;
   mat A = initialize(N);
@@ -10,14 +10,19 @@ TEST_CASE("Testing Maximum Values"){
   REQUIRE(*k==2);
   REQUIRE(*l==1);
 }
+*/
 TEST_CASE("Testing Eigenvalues"){
-  int N = 3;
-  mat A = initialize(N);
-  iterative(A, N);
+  int N = 4;
+  potential = false;
+  max = 1;
+  mat A = initialize(N, max, potential);
+  mat R(N,N,fill::eye);
+  iterative(A, R, N);
   vec eigen = get_eigvals(A, N);
-  REQUIRE(eigen(0) ==Approx(5.2721));
-  REQUIRE(eigen(1) == Approx(18.0000));
-  REQUIRE(eigen(2) == Approx(30.7279));
+  REQUIRE(eigen(0) ==Approx(6.1115));
+  REQUIRE(eigen(1) == Approx(22.1115));
+  REQUIRE(eigen(2) == Approx(41.8885));
+  REQUIRE(eigen(3) == Approx(57.8885));
 }
 
 
