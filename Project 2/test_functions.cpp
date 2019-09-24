10c55b4fa6eba1e84a6e2dedc16e1f01f213f3f3
ilseku <54395297+ilseku@users.noreply.github.com>
2019-09-24 15:34:36 +0200

    Update test_functions.cpp

--- Project 2/test_functions.cpp
+++ Project 2/test_functions.cpp
@@ -1,28 +1,6 @@
 #include "catch.hpp"
 #include "Project2.h"
 
-TEST_CASE("Testing Eigenvector Orthogonality"){
-  int N = 3;
-  bool potential = false;
-  double max = 1.0;
-  mat A = initialize(N, max, potential);
-  mat R(N,N,fill::eye);
-  iterative(A, R, N);
-  //dot product of vector 1 and vector 2 should be zero
-  double dot01 = R(0,0)*R(1,0) +R(0,1)*R(1,1) + R(0,2)*R(1,2);
-  double dot12 = R(1,0)*R(2,0) +R(1,1)*R(2,1) + R(1,2)*R(2,2);
-  double dot20 = R(2,0)*R(0,0) +R(2,1)*R(0,1) + R(2,2)*R(0,2);
-  //dot product of vector 1 with vector 1 should be one
-  double dot00 = R(0,0)*R(0,0) + R(0,1)*R(0,1) + R(0,2)*R(0,2);
-  double dot22 = R(2,0)*R(2,0) + R(2,1)*R(2,1) + R(2,2)*R(2,2);
-  REQUIRE(dot01 == Approx(0.000).epsilon(0.01));
-  REQUIRE(dot12 == Approx(0.000).epsilon(0.01));
-  REQUIRE(dot20 == Approx(0.000).epsilon(0.01));
-  REQUIRE(dot00 == Approx(1.000).epsilon(0.01));
-  REQUIRE(dot22 == Approx(1.000).epsilon(0.01));
-}
-
-
 TEST_CASE("Testing Eigenvalues"){
   int N = 4;
   bool potential = false;
@@ -36,3 +14,25 @@ TEST_CASE("Testing Eigenvalues"){
   REQUIRE(eigen(2) == Approx(41.8885));
   REQUIRE(eigen(3) == Approx(57.8885));
 }
+
+TEST_CASE("Testing Eigenvector Orthogonality"){
+  int N = 4;
+  bool potential = false;
+  double max = 1.0;
+  mat A = initialize(N, max, potential);
+  mat R(N,N,fill::eye);
+  iterative(A, R, N);
+  //dot product of vector 1 and vector 2 should be zero
+  double dot01 = R(0,0)*R(1,0) +R(0,1)*R(1,1) + R(0,2)*R(1,2) + R(0,3)*R(1,3);
+  double dot12 = R(1,0)*R(2,0) +R(1,1)*R(2,1) + R(1,2)*R(2,2) + R(1,3)*R(2,3);
+  double dot20 = R(2,0)*R(0,0) +R(2,1)*R(0,1) + R(2,2)*R(0,2) + R(2,3)*R(0,3);
+  //dot product of vector 1 with vector 1 should be one
+  double dot00 = R(0,0)*R(0,0) + R(0,1)*R(0,1) + R(0,2)*R(0,2) + R(0,3)*R(0,3);
+  double dot22 = R(2,0)*R(2,0) + R(2,1)*R(2,1) + R(2,2)*R(2,2) + R(2,3)*R(2,3);
+  REQUIRE(dot01 == Approx(0.000).epsilon(0.01));
+  REQUIRE(dot12 == Approx(0.000).epsilon(0.01));
+  REQUIRE(dot20 == Approx(0.000).epsilon(0.01));
+  REQUIRE(dot00 == Approx(1.000).epsilon(0.01));
+  REQUIRE(dot22 == Approx(1.000).epsilon(0.01));
+}
+
