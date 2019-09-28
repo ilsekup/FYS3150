934f3f3164ae412284e01c7879f1e6fddf6035ac
ilseku <54395297+ilseku@users.noreply.github.com>
2019-09-28 17:36:31 +0200

    Update Project2.h

--- Project 2/Project2.h
+++ Project 2/Project2.h
@@ -10,7 +10,7 @@
 #include <string>
 #include <armadillo>
 
-std::ofstream ofile;
+
 using namespace std;
 using namespace arma;
 
@@ -19,10 +19,9 @@ double maxoff(mat&, mat&, int, int *, int *);
 void jacobi_method(mat&, int, int, int);
 void iterative(mat&, mat&, int);
 mat initialize(int, double, bool);
-vec get_eigvals(mat&, int);
 void print_file(mat&, vec&, double, int);
 double get_error(vec, vec, int);
 vec analytic_eigvals_harmonic(int);
-void sort_eigenproblem(mat &, vec &, int);
+vec sort_eigenproblem(mat &, mat &, int);
 
 #endif
