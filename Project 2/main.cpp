7477515b0807e46e0d608bb8d308e9db5975e6c8
ilseku <54395297+ilseku@users.noreply.github.com>
2019-09-28 17:37:45 +0200

    Update main.cpp

--- Project 2/main.cpp
+++ Project 2/main.cpp
@@ -13,6 +13,8 @@
 int main(int argc, char *argv[]){
   using namespace std;
   using namespace arma;
+  clock_t start, finish;
+  double time_spent;
   int N = atof(argv[1]);
   double max = 1.0; //when we dont have a potential rho[N-1] = 1, when we do rho[N-1] =infinity
   mat A = initialize(N, max, false);
@@ -22,14 +24,26 @@ int main(int argc, char *argv[]){
   double d = 2.0/hh; //diagonal elements
   double a = -1/hh;
   //A.print("A = ");
+  start = clock();
   vec eigenvalues_arma = eig_sym(A);
+  finish = clock();
+  time_spent = ( (double)(finish - start)/ CLOCKS_PER_SEC );
+  cout << "time for amamadillo eigenvlaues: " << time_spent << " s" << endl;
   //eigenvalues_arma.print("Eigenvalues (from armadillo) = ");
+  start = clock();
   vec eigenvalues_analytical = analytic_eigvals(N,d,a); //this only shows the eigenvalues with no potential
-  //eigenvalues_analytical.print("Analytical eigenvalues = ");
+  finish = clock();
+  time_spent = ( (double)(finish - start)/ CLOCKS_PER_SEC );
+  cout << "Time to calculate analytically: " << time_spent << " s" << endl;
+  eigenvalues_analytical.print("Analytical eigenvalues = ");
   vec eigenvalues_analytical_potential = analytic_eigvals_harmonic(N);
+  start = clock();
   iterative(A,R,N);
-  vec eigenvalues_Jacobi = get_eigvals(A,N);
+  vec eigenvalues_Jacobi = sort_eigenproblem(A, R, N);
   eigenvalues_Jacobi.print("Eigenvalues (Jacobi) = ");
+  finish = clock();
+  time_spent = ( (double)(finish - start)/ CLOCKS_PER_SEC );
+  cout << "Time spend with jacobi_method: " << time_spent << " s" << endl;
   //R.print("R = ");
   //A.print("A = ");
   double average_error = get_error(eigenvalues_analytical, eigenvalues_Jacobi, N);
