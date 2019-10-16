da17ad6a37bf3bcba42fc4905bfef5c0311b26c6
fredln <fredriln@student.matnat.uio.no>
2019-10-16 15:43:10 +0200

    MonteCarlo now takes the uniform limits as arguments

--- Project 3/MonteCarlo.cpp
+++ Project 3/MonteCarlo.cpp
@@ -10,42 +10,33 @@
 
 using namespace std;
 
-// The function to integrate
-//double func(double*);
-
-uniform_transform::uniform_transform(double a1, double b1){
-  a = a1;
-  b = b1;
-  diff = b-a;
-}
-
-double uniform_transform::transform(double x){
-  return a + x*diff;
-}
-
-double MonteCarlo(double (*func)(double*), FunctionPointers* transforms, int N, int d){
-    //set engine
-    uniform_real_distribution<double> distribution(0,1);
+double MonteCarlo(double (*func)(double* ), double* a, double* b, int N, int d){
+    uniform_real_distribution<double> distribution(0.0,1.0);
     ranlux48 generator;
     generator.seed(time(NULL));
     auto random = bind(distribution,generator);
 
+    // Define arrays for a-b and the random values for the position vector r
+    double* diff = new double[d];
     double* r = new double[d];
+    double jacobian = 1.0;
+
+    for(int i=0;i<d;i++){
+      diff[i] = b[i] - a[i];
+      jacobian *= diff[i];
+    }
+
     double sum = 0;
 
     for(int i=0; i<N;i++){
         for(int j=0;j<d;j++){
-            r[j] = (transforms[j])(random());
+            r[j] = a[j] + random()*diff[j];
         }
         sum += func(r);
     }
     delete [] r;
+    delete [] diff;
+
+    sum *= jacobian;
     return sum / (double) N;
 }
-
-/*
-double uniform(){
-    uniform_real_distribution<double> distribution(-inf,inf);
-    return distribution(engine);
-}
-*/
--- Project 3/MonteCarlo.h
+++ Project 3/MonteCarlo.h
@@ -11,24 +11,12 @@
 
 using namespace std;
 
-typedef double(*FunctionPointers)(double);
-
 // Preforms MonteCarlo integration
 // Takes:
 // a function to integrate
 // a random number function, should take a number between 0 and 1 and return a double
 // The number of points to calculate
 // and the dimensions of the function
-double MonteCarlo(double (*)(double*), FunctionPointers*, int, int);
-
-// Class to transform a random number between 0 and 1, to one between a and b
-class uniform_transform {
-  double a; // start of the uniform distribution
-  double b; // stop of the uniform distribution
-  double diff; // b-a, saved as variable to save time
-public:
-  uniform_transform(double, double);
-  double transform(double);
-};
+double MonteCarlo(double (*)(double* ), double* , double* , int, int);
 
 #endif
