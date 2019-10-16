e58fb7063309b3340ee28fde1f5396e8921760d9
ilseku <54395297+ilseku@users.noreply.github.com>
2019-10-16 19:16:08 +0200

    Update main.cpp

--- Project 3/main.cpp
+++ Project 3/main.cpp
@@ -10,11 +10,15 @@
 #include "Gauss.cpp"
 
 int main(){
-  int N;
-  double lam;
-  cout << "Enter interger for N: " << endl;
-  cin >> N;
-  cout << "Enter double for lambda: " << endl;
-  cin >> lam;
-  double integral = calculate(N, lam);
+   int N;
+   double lam;
+   cout << "Enter interger for N: " << endl;
+   cin >> N;
+   cout << "Enter double for lambda: " << endl;
+   cin >> lam;
+   for(int i = 1; i < 3; i++){
+      double integral = Gausslegendre(N*i, lam);
+      double integrallag = Gausslaguerre(N*i);
+}
+
 }
