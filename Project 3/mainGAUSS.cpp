ad8f4c24bd42595628dc06fd957bddd7458fa4ce
ilseku <54395297+ilseku@users.noreply.github.com>
2019-10-18 10:27:53 +0200

    Update mainGAUSS.cpp

--- Project 3/mainGAUSS.cpp
+++ Project 3/mainGAUSS.cpp
@@ -16,9 +16,27 @@ int main(){
    cin >> N;
    cout << "Enter double for lambda: " << endl;
    cin >> lam;
-   for(int i = 1; i < 3; i++){
-      double integral = Gausslegendre(N*i, lam);
-      double integrallag = Gausslaguerre(N*i);
-}
+   clock_t start, finish;
+   double time_spent;
+   for(int i = 1; i <= 3; i++){
+    start =clock();
+    double int_gauss = Gausslegendre(N*i, lam);
+    finish =clock();
+    time_spent = ( (double)(finish - start)/ CLOCKS_PER_SEC );
+    cout << "Legendre method with N= "<< N*i << endl;
+    cout <<"Integral: " <<int_gauss << endl;
+    double Error = 0.19277 - int_gauss;
+    cout <<"Error: " << Error << endl;
+    cout << "Time spent for legendre with " << N*i << " iterations is " << time_spent << endl;
+    start = clock();
+    double int_gausslag = Gausslaguerre(N*i);
+    finish  =clock();
+    time_spent = ( (double)(finish - start)/ CLOCKS_PER_SEC );
+    cout << "Laguerre method with N= "<< N*i << endl;
+    cout <<"Integral: " <<int_gausslag << endl;
+    double Errorlag = abs(0.19277 - int_gausslag);
+    cout <<"Error: " << Errorlag << endl;
+    cout << "Time spent for laguerre with " << N*i << " iterations is " << time_spent << endl;
+    }
 
 }
