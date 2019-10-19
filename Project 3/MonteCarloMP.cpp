6ebbf11dedb18150c46196174d8a3c43952da81a
Fevzi1508 <54434831+Fevzi1508@users.noreply.github.com>
2019-10-19 16:25:31 +0200

    Program works but not really
    Gives nan for importance sampling and weird times (probably due to different threads).
    
    $g++ -o MC mainMCwithtime.cpp MonteCarloMP.cpp -fopenmp

--- Project 3/MonteCarloMP.cpp
+++ Project 3/MonteCarloMP.cpp
@@ -7,25 +7,31 @@
 #include "MonteCarlo.h"
 #include <random>
 #include <functional>
+#include <omp.h>
 
 using namespace std;
 using namespace std::placeholders;
 
-array<double, 2> MonteCarlo(std::function<double(double*)> func, double* a, double* b, int N, int d){
+array<double, 2> MonteCarlo(std::function<double(double*)> func, double* a, double* b, int N, int d)
+{
+
   double sum = 0, sum_sqr = 0;
   double jacobian = 1.0;
   double* diff = new double[d];
-  // Calculate the jacobian
 
-  for(int i=0;i<d;i++){
+  // Calculate the jacobian
+  for(int i=0;i<d;i++)
+      {
     diff[i] = b[i] - a[i];
     jacobian *= diff[i];
-  }
-#pragma omp parallel shared(sum, sum_sqr,jacobian)
+      }
+  int p = 0; // thread number
+#pragma omp parallel shared(sum, sum_sqr,jacobian,diff) private(p)
   {
-    uniform_real_distribution<double> distribution(0.0,1.0);
+    p = omp_get_thread_num();
+    uniform_real_distribution<double>distribution(0.0,1.0);
     mt19937_64 generator;
-    generator.seed(time(NULL));//+ 10*omp_get_thread_num());
+    generator.seed(time(NULL) + 10 * p);
     auto random = bind(distribution,generator);
 
     // Define arrays for a-b and the random values for the position vector r
@@ -33,11 +39,13 @@ array<double, 2> MonteCarlo(std::function<double(double*)> func, double* a, doub
 
     double fr;
     // Find the sum f(x_i), and the sum of f(x_i^2)
-    #pragma omp for reduction(+:sum) reduction(+:sum_sqr)
-    for(int i=0; i<N;i++){
-        for(int j=0;j<d;j++){
-            r[j] = a[j] + random()*diff[j];
-        }
+    #pragma omp for reduction(+:sum,sum_sqr)
+    for(int i=0; i<N;i++)
+    {
+      for(int j=0;j<d;j++)
+      {
+       r[j] = a[j] + random()*diff[j];
+      }
         fr = func(r);
         sum += fr;
         sum_sqr += fr*fr;
