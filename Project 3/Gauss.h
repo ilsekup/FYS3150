13275c2faad2105008c5cb0d72b053fe3426582f
ilseku <54395297+ilseku@users.noreply.github.com>
2019-10-20 13:35:43 +0200

    Update Gauss.h

--- Project 3/Gauss.h
+++ Project 3/Gauss.h
@@ -15,9 +15,9 @@ using namespace std;
 using namespace arma;
 void gauleg(double, double, double *, double *, int);
 double int_function(double, double, double, double, double , double );
-double Gausslegendre(int, double);
+tuple<double, double> Gausslegendre(int, double);
 double gauss_laguerre(double *, double *, double, int);
-double Gausslaguerre(int N);
+tuple<double, double> Gausslaguerre(int N);
 double int_func_sphere(double, double, double, double, double, double);
 
 
