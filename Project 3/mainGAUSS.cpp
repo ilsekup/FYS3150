67bf94709a3847c75983b7a7d69b42852c444d7c
ilseku <54395297+ilseku@users.noreply.github.com>
2019-10-20 13:35:47 +0200

    Update mainGAUSS.cpp

--- Project 3/mainGAUSS.cpp
+++ Project 3/mainGAUSS.cpp
@@ -16,27 +16,32 @@ int main(){
    cin >> N;
    cout << "Enter double for lambda: " << endl;
    cin >> lam;
+   double integral, weight;
+  //  tie(integral, weight) = Gausslegendre(N, lam)
+  //  tie(integrallag, weightlag)= Gausslaguerre(N);
    clock_t start, finish;
    double time_spent;
    for(int i = 1; i <= 3; i++){
-    start =clock();
-    double int_gauss = Gausslegendre(N*i, lam);
-    finish =clock();
-    time_spent = ( (double)(finish - start)/ CLOCKS_PER_SEC );
-    cout << "Legendre method with N= "<< N*i << endl;
-    cout <<"Integral: " <<int_gauss << endl;
-    double Error = 0.19277 - int_gauss;
-    cout <<"Error: " << Error << endl;
-    cout << "Time spent for legendre with " << N*i << " iterations is " << time_spent << endl;
-    start = clock();
-    double int_gausslag = Gausslaguerre(N*i);
-    finish  =clock();
-    time_spent = ( (double)(finish - start)/ CLOCKS_PER_SEC );
-    cout << "Laguerre method with N= "<< N*i << endl;
-    cout <<"Integral: " <<int_gausslag << endl;
-    double Errorlag = abs(0.19277 - int_gausslag);
-    cout <<"Error: " << Errorlag << endl;
-    cout << "Time spent for laguerre with " << N*i << " iterations is " << time_spent << endl;
+     start = clock();
+     double int_gauss, weight;
+     tie(int_gauss, weight) = Gausslegendre(N*i, lam);
+     finish = clock();
+     time_spent = ( (double)(finish - start)/ CLOCKS_PER_SEC );
+     cout << "Legendre method with N= "<< N*i << endl;
+     cout <<"Integral: " <<int_gauss << endl;
+     double Error = 0.19277 - int_gauss;
+     cout <<"Error: " << Error << endl;
+     cout << "Time spent for legendre with " << N*i << " iterations is " << time_spent << endl;
+     start = clock();
+     double int_gausslag, weightlag;
+     tie(int_gausslag, weightlag) = Gausslaguerre(N*i);
+     finish = clock();
+     time_spent = ( (double)(finish - start)/ CLOCKS_PER_SEC );
+     cout << "Laguerre method with N= "<< N*i << endl;
+     cout <<"Integral: " <<int_gausslag << endl;
+     double Errorlag = abs(0.19277 - int_gausslag);
+     cout <<"Error: " << Errorlag << endl;
+     cout << "Time spent for laguerre with " << N*i << " iterations is " << time_spent << endl;
     }
 
 }
