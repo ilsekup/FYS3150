d1e8f88cbbd1f6df99d1ec9c993f2bc85aeea9d2
ilseku <54395297+ilseku@users.noreply.github.com>
2019-11-12 22:06:37 +0100

    Update plottingising.py

--- Project 4/plottingising.py
+++ Project 4/plottingising.py
@@ -46,9 +46,11 @@ C = np.array(heatcapacity)
 # plt.ylabel("sigma_E ^2/ T^2")
 # plt.grid()
 # plt.show()
+# mc =  [500, 1000, 1500, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 30000, 40000,50000, 100000]
+mc = np.arange(0, 9500, step=500)
+Etemp1 = np.zeros(len(mc)); Mtemp1 =np.zeros(len(mc)); Etemp24 = np.zeros(len(mc)); Mtemp24 =np.zeros(len(mc))
+Etemp1ran = np.zeros(len(mc)); Mtemp1ran =np.zeros(len(mc))
 
-mc = [1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7]
-Etemp1 = np.zeros(len(mc)); Mtemp1 =np.zeros(len(mc))
 for i, s in enumerate(mc):
     filename = "outfile%g.txt" %(s)
     fil = open(filename, 'r') #reading file
@@ -62,17 +64,43 @@ for i, s in enumerate(mc):
     fil.close()
     E = np.array(E)
     M = np.array(M)
-    Etemp1[i]  = E[0]
-    Mtemp1[i] = M[0]
+    Etemp1[i]  = E[0];
+    Etemp24[i] = E[14]
+    Mtemp1[i] = M[0];
+    Mtemp24[i] = M[14]
+for i, s in enumerate(mc):
+    filename = "outfileran%g.txt" %(s)
+    fil = open(filename, 'r') #reading file
+    lines = fil.readlines() #splitting in lines
+    length  = len(lines)
+    E = []
+    M = []
+    for line in (lines):
+        E.append(float(line.split()[1]))
+        M.append(float(line.split()[-1]))
+    fil.close()
+    E = np.array(E)
+    M = np.array(M)
+    Etemp1ran[i]  = E[0]
+    Mtemp1ran[i] = M[0]
 mc = np.array(mc)
 fig, axs = plt.subplots(2, sharex=True)
 fig.suptitle('Temperature 1 ')
-axs[0].plot(mc, Etemp1)
+axs[0].plot(mc, Etemp24, label='order')
+# axs[0].plot(mc, Etemp1ran, label='random')
 axs[0].set_title('Energy')
-axs[1].plot(mc, Mtemp1)
+axs[0].legend()
+axs[1].plot(mc, Mtemp24, label='ordered')
+# axs[1].plot(mc, Mtemp1ran, label='random')
 axs[1].set_title('Magnetization')
-plt.xticks(mc)
-plt.xscale('log')
+axs[1].legend()
+# plt.xticks(mc)
+# plt.xscale('log')
 
 #plt.ticklabel_format(useOffset=False, style='plain')
+plt.show()
+# plt.plot(Etemp1, np.linspace(0, 19, 19))
+# plt.show()
+plt.plot(mc, Etemp1)
+print(Etemp1)
 plt.show()
