233552dc67c3da5e133395226c2dc1a2a1684664
ilseku <54395297+ilseku@users.noreply.github.com>
2019-11-12 18:58:26 +0100

    Update plottingising.py

--- Project 4/plottingising.py
+++ Project 4/plottingising.py
@@ -26,23 +26,53 @@ T = np.array(T)
 M = np.array(M)
 C = np.array(heatcapacity)
 
-plt.plot(T, E)
-plt.title('Mean Energy')
-plt.xlabel("Temperature")
-plt.ylabel("Mean energy/n^2")
-plt.grid()
-plt.show()
+# plt.plot(T, E)
+# plt.title('Mean Energy')
+# plt.xlabel("Temperature")
+# plt.ylabel("Mean energy/n^2")
+# plt.grid()
+# plt.show()
+#
+# plt.plot(T, M)
+# plt.title('Abs Mean Magnetization')
+# plt.xlabel("Temperature")
+# plt.ylabel("Mean absolute Magnetization/n^2")
+# plt.grid()
+# plt.show()
+#
+# plt.plot(T, C)
+# plt.title('heatcapacity')
+# plt.xlabel("Temperature")
+# plt.ylabel("sigma_E ^2/ T^2")
+# plt.grid()
+# plt.show()
 
-plt.plot(T, M)
-plt.title('Abs Mean Magnetization')
-plt.xlabel("Temperature")
-plt.ylabel("Mean absolute Magnetization/n^2")
-plt.grid()
-plt.show()
+mc = [1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7]
+Etemp1 = np.zeros(len(mc)); Mtemp1 =np.zeros(len(mc))
+for i, s in enumerate(mc):
+    filename = "outfile%g.txt" %(s)
+    fil = open(filename, 'r') #reading file
+    lines = fil.readlines() #splitting in lines
+    length  = len(lines)
+    E = []
+    M = []
+    for line in (lines):
+        E.append(float(line.split()[1]))
+        M.append(float(line.split()[-1]))
+    fil.close()
+    E = np.array(E)
+    M = np.array(M)
+    Etemp1[i]  = E[0]
+    Mtemp1[i] = M[0]
+mc = np.array(mc)
+fig, axs = plt.subplots(2, sharex=True)
+fig.suptitle('Temperature 1 ')
+axs[0].plot(mc, Etemp1)
+axs[0].set_title('Energy')
+axs[1].plot(mc, Mtemp1)
+axs[1].set_title('Magnetization')
+plt.xticks(mc)
+plt.xscale('log')
 
-plt.plot(T, C)
-plt.title('heatcapacity')
-plt.xlabel("Temperature")
-plt.ylabel("sigma_E ^2/ T^2")
-plt.grid()
+#plt.ticklabel_format(useOffset=False, style='plain')
 plt.show()
