c232362e5336903d14ea59e16351311d1bc5da90
ilseku <54395297+ilseku@users.noreply.github.com>
2019-11-15 16:44:07 +0100

    Update main.cpp

--- Project 4/main.cpp
+++ Project 4/main.cpp
@@ -18,6 +18,7 @@ int main(int argc, char* argv[]){
   outfilename = argv[1];
   ofile.open(outfilename);
   ofile2.open("temp.txt");
+  ofile3.open("histdata.txt");
   cout << "Insert L, dimension of matrix: " << endl;
   cin >> n;
   cout << "Number of monte carlo cycles" << endl;
@@ -31,13 +32,13 @@ int main(int argc, char* argv[]){
   long startpoint = -1;
   clock_t start, finish;
   double time_spent;
+  long seed = time(NULL);
   start = clock();
   for(double T= initial_T; T <= final_T; T+=T_step){
     E = M = 0;
     for(int de=-8; de <= 8; de++) w[de+8] = 0;
     for(int de =-8; de <= 8; de+=4)w[de+8] = exp(-de/T);
     for (int i = 0; i < 5; i++) average[i] = 0.;
-    long seed = time(NULL);
     initialize(n,  T, spin_matrix, E, M, seed, false);  //false if we want random spins, true for ordered
     for(int cycles=1; cycles <= mc; cycles++){
       metropolis(n, startpoint, spin_matrix, E, M, w);
@@ -57,24 +58,36 @@ int main(int argc, char* argv[]){
     for(int de2=-8; de2 <= 8; de2++) w[de2+8] = 0;
     for(int de2 =-8; de2 <= 8; de2+=4)w[de2+8] = exp(-de2/Tlone);
     for (int i = 0; i < 5; i++) energy[i] = 0.;
-    long seed = time(NULL);
     initialize(n,  Tlone, spin_matrix, E2, M2, seed, true);  //false if we want random spins, true for ordered
     for(int cycles= 1; cycles <= sweep; cycles++){
       metropolis(n, startpoint, spin_matrix, E2, M2, w);
       energy[0] += E2;
       energy[1] += E2*E2;
       energy[3] += fabs(M2);
+
     }
-    cout << sweep << endl;
     writingfunc2(n, sweep, Tlone, energy);
   }
 }
+E = M = 0;
+for(int de=-8; de <= 8; de++) w[de+8] = 0;
+for(int de =-8; de <= 8; de+=4)w[de+8] = exp(-de/Tlone);
+for (int i = 0; i < 5; i++) average[i] = 0;
+initialize(n,  Tlone, spin_matrix, E, M, seed, false);  //false if we want random spins, true for ordered
+  for(int cycles=1; cycles <= mc; cycles++){
+    metropolis(n, startpoint, spin_matrix, E, M, w);
+    double energy = E;
+    average[0] += E;
+    average[1] += E*E;
+    writingfunc3(n, mc, Tlone, energy, average, cycles);
+    }
   finish = clock();
   time_spent = ( (double)(finish - start)/ CLOCKS_PER_SEC );
   cout << "Time spent: " << time_spent << endl; //time spent on the loops and writing to file
   free_matrix((void**)spin_matrix); // freeing memory
   ofile2.close();
   ofile.close();
+  ofile3.close();
 
   return 0;
 }
