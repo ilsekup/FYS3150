238b60359eef9da30bbed8c3bad7cbd456b7dc3d
ilseku <54395297+ilseku@users.noreply.github.com>
2019-11-15 16:44:03 +0100

    Update isingmodel.h

--- Project 4/isingmodel.h
+++ Project 4/isingmodel.h
@@ -9,9 +9,11 @@
 #include "time.h"
 #include <string>
 
-void initialize(int, double, int **, double&, double&, bool);
+void initialize(int, double, int **, double&, double&, long, bool);
 inline int periodic(int, int, int);
 void metropolis(int, long&, int**, double&, double&, double*);
 void writingfunc(int, int, double, double*);
 void writingfunc2(int , int , double , double*, double *);
+void writingfunc3(int, int, double, double, double *, int);
+
 #endif
