f74375eea2b2fbfec4f063cbc2aef21626bc766c
Fevzi1508 <54434831+Fevzi1508@users.noreply.github.com>
2019-11-15 14:31:00 +0100

    New plotting thingy

--- Project 4/histogram.py
+++ Project 4/histogram.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
-#compile >> g++ maind.cpp lib.cpp << and run the program with CLA "histdata.txt"
+
+#compile >> g++ maind.cpp lib.cpp << and run
+#this program. It will ask for the filename and temp to plot histogram.
+
 filename = str(input("Filename : "))
 fil = open(filename, 'r') #reading file
 lines = fil.readlines() #splitting in lines
@@ -8,17 +11,21 @@ length  = len(lines)
 T = float(input("T ="))
 
 E = []
+
 for line in lines:
     E.append(float(line.split()[0]))
 fil.close()
 
-#each MC cycle represent a "time step"
 E = np.array(E)
+#each MC cycle represent a "time step"
 
 #assuming that the energy is stabilized after 4000 MCc
-E_stabilized = E[4000:-1] #
-plt.figure(figsize = (8,6))
+#based on the plots from previou
+E_stabilized = E[4000:-1]
+
 #plotting
+plt.figure(figsize = (8,6))
+
 plt.title("Histogram showing energy probability P(E) per particle   \n \
 20 x 20 lattice, T = %.1f   " % T,fontsize = 13)
 plt.xlabel("Energy per particle E / N",fontsize = 13)
@@ -31,6 +38,7 @@ weights = np.ones_like(E_stabilized)/float(len(E_stabilized))
 plt.hist(E_stabilized, bins = 30, color = 'blue', edgecolor = 'black'\
 ,normed = 0, weights=weights)
 #normalized histogram of energies
+
 plt.show()
 
 """
