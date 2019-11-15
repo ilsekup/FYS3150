7211b47ef0ef9c4ae00957a15c6bbc714bc9a347
Fevzi1508 <54434831+Fevzi1508@users.noreply.github.com>
2019-11-15 14:28:06 +0100

    new histogram plotting program

--- Project 4/histogram.py
+++ Project 4/histogram.py
@@ -1,45 +1,40 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
-filename = "histdata.txt"
+#compile >> g++ maind.cpp lib.cpp << and run the program with CLA "histdata.txt"
+filename = str(input("Filename : "))
 fil = open(filename, 'r') #reading file
 lines = fil.readlines() #splitting in lines
 length  = len(lines)
 T = float(input("T ="))
 
-
-MCc = []
 E = []
 for line in lines:
-    MCc.append(float(line.split()[0]))
-    E.append(float(line.split()[1]))
+    E.append(float(line.split()[0]))
 fil.close()
 
 #each MC cycle represent a "time step"
 E = np.array(E)
-MCc = np.array(MCc)
 
+#assuming that the energy is stabilized after 4000 MCc
+E_stabilized = E[4000:-1] #
 plt.figure(figsize = (8,6))
-plt.title("Energy plotted against MC cycles  \n \
-20 x 20 lattice, T = %.1f   " % T,fontsize = 12)
-plt.xlabel("MC cycles",fontsize = 12)
-plt.ylabel("Energy per particle E / N", fontsize = 12)
-plt.xticks(fontsize = 12)
-plt.yticks(fontsize = 12)
-plt.plot(MCc[0::1500],E[0::1500])
+#plotting
+plt.title("Histogram showing energy probability P(E) per particle   \n \
+20 x 20 lattice, T = %.1f   " % T,fontsize = 13)
+plt.xlabel("Energy per particle E / N",fontsize = 13)
+plt.ylabel("Probability P(E) / N", fontsize = 13)
+plt.xticks(fontsize = 13)
+plt.yticks(fontsize = 13)
 
-
-#assuming that the energy is stabilized after 3000 MCc
-E_stabilized = E[3000:-1]
-plt.figure(figsize = (8,6))
-plt.title("Histogram showing Probability density P(E) per particle   \n \
-20 x 20 lattice, T = %.1f   " % T,fontsize = 12)
-plt.xlabel("Energy per particle E / N",fontsize = 12)
-plt.ylabel("Probability density P(E) / N", fontsize = 12)
-plt.xticks(fontsize = 12)
-plt.yticks(fontsize = 12)
-
+bins = 30
 weights = np.ones_like(E_stabilized)/float(len(E_stabilized))
-plt.hist(E_stabilized, bins = 30, color = 'blue', edgecolor = 'black',normed = 0, weights=weights)
-#normalized histogram
+plt.hist(E_stabilized, bins = 30, color = 'blue', edgecolor = 'black'\
+,normed = 0, weights=weights)
+#normalized histogram of energies
 plt.show()
+
+"""
+sample run
+Terminal > histogram.py
+(output is a plot)
+"""
