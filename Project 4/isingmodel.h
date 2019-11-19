6c096298afd763df7c4f734f75651164c777eb7a
Fevzi1508 <54434831+Fevzi1508@users.noreply.github.com>
2019-11-19 18:12:39 +0100

    Small change cause of counter

--- Project 4/isingmodel.h
+++ Project 4/isingmodel.h
@@ -12,9 +12,9 @@ using namespace std;
 
 void initialize(int, double, int **, double&, double&, long, bool);
 inline int periodic(int, int, int);
-void metropolis(int, long&, int**, double&, double&, double*);
+void metropolis(int, long&, int**, double&, double&, double*, int&);
 void writingfunc(int, int, double, double*, ostream&);
-void writingfunc2(int , int , double , double*, ostream&);
+void writingfunc2(int , int , double , double*,int, ostream&);
 void writingfunc3(int, int, double, double, double *, int, ostream&);
 
 #endif
