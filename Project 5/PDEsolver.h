677d9d8f48025ecbcb4b74159e4e8c9b9db455d6
Fevzi1508 <54434831+Fevzi1508@users.noreply.github.com>
2019-11-26 14:48:46 +0100

    Commit

---
+++ Project 5/PDEsolver.h
@@ -0,0 +1,29 @@
+#ifndef PDESOLVER_H
+#define PDESOLVER_H
+
+
+
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <fstream>
+#include <iomanip>
+#include "time.h"
+#include <string>
+using namespace std;
+using namespace arma;
+
+
+void writingfunc1D(int, vec, ostream&)
+void writingfunc2D(int,mat, ostream&)
+
+void explicitsch1D(int, int)
+void explicitsch2D(int, int)
+
+
+
+
+
+
+
+#endif
