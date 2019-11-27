9cf8d587849b44d8aa9e622adc9cf889f17cb98b
ilseku <54395297+ilseku@users.noreply.github.com>
2019-11-27 21:38:52 +0100

    Update PDEsolver.h

--- Project 5/PDEsolver.h
+++ Project 5/PDEsolver.h
@@ -1,8 +1,5 @@
 #ifndef PDESOLVER_H
 #define PDESOLVER_H
-
-
-
 #include <iostream>
 #include <cmath>
 #include <cstdlib>
@@ -10,20 +7,17 @@
 #include <iomanip>
 #include "time.h"
 #include <string>
+#include<armadillo>
+
 using namespace std;
 using namespace arma;
 
-
-void writingfunc1D(int, vec, ostream&)
-void writingfunc2D(int,mat, ostream&)
-
-void explicitsch1D(int, int)
-void explicitsch2D(int, int)
-
-
-
-
-
+void writingfunc1D(int, vec, ostream&);
+void writingfunc2D(int,mat, ostream&);
+void explicitsch1D(int, int);
+void explicitsch2D(int, int);
+double *implicit(int, double);
+void solver_Thomas(double*, double *, double *, double *, double *, int);
 
 
 #endif
