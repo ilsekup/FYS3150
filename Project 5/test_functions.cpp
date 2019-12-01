32fe090674052492b0e72d4497c6581e738a9422
fredln <fredriln@student.matnat.uio.no>
2019-12-01 17:08:22 +0100

    Updated to fit with new functions

--- Project 5/test_functions.cpp
+++ Project 5/test_functions.cpp
@@ -5,8 +5,10 @@
 TEST_CASE("Testing the initial conditions implicit scheme"){
   int n = 10;
   int t_steps = 5;
+  double dx = 1/(double) (n+1);
+  double dt = 0.5*dx*dx;
   for(int i = 0; i < t_steps; i++){
-    double *vin = implicit(n, t_steps); //checking that the boundary conditions hold for all different timesteps
+    double *vin = implicit(n, dt, t_steps); //checking that the boundary conditions hold for all different timesteps
     REQUIRE(vin[0] == Approx(0));
     REQUIRE(vin[n+1] == Approx(1));
   }
