9cb3e487a586065cd48ce745f311e1113e9352f6
Fevzi1508 <54434831+Fevzi1508@users.noreply.github.com>
2019-12-03 15:17:32 +0100

    Added pause function

--- Project 5/plottingdiff.py
+++ Project 5/plottingdiff.py
@@ -21,27 +21,38 @@ x = np.linspace(0,1, len(u_xx[0]))
 #Making an animation to see temporal evolution
 #when running the file you get from PDEsolver.cpp
 #Try n = 100, t = 20 000 and running this code
-fig = plt.figure(figsize = (16,8))
+fig = plt.figure(figsize = (10,6))
 ax = plt.axes(xlim=(0, 1), ylim=(0, 1))
 line, = ax.plot([], [], lw=2)
+
+
+pause = False
 
 def init():
     line.set_data([], [])
     return line,
 
 def animate(i):
-    x = np.linspace(0,1, len(u_xx[0]))
-    y = u_xx[i]
-    plt.title("Temporal evolution of density u as a function of the x-position time step t = %i"  % i\
-    , fontsize = 12)
-    line.set_data(x, y)
+     if not pause:
+        x = np.linspace(0,1, len(u_xx[0]))
+        y = u_xx[i]
+        plt.title("Temporal evolution of density u as a function of the x-position time step t = %i"  % i\
+        , fontsize = 15)
+        line.set_data(x, y)
+        return line,
 
-    return line,
+
+def onClick(event):
+    global pause
+    pause ^= True
 
 anim = animation.FuncAnimation(fig, animate, init_func=init,
                                frames=2000, interval=200, blit=False)
-plt.xlabel("x-position", fontsize = 12)
-plt.ylabel("u density", fontsize = 12)
-plt.xticks(fontsize = 12)
-plt.yticks(fontsize = 12)
+                               
+fig.canvas.mpl_connect('button_press_event', onClick)
+plt.xlabel("x-position", fontsize = 15)
+plt.ylabel("u density", fontsize = 15)
+plt.xticks(fontsize = 15)
+plt.yticks(fontsize = 15)
+plt.tight_layout()
 plt.show()
