7cf517471fa5cf2a96fcbf22bd1e4dbed6128d26
ilseku <54395297+ilseku@users.noreply.github.com>
2019-12-07 13:44:33 +0100

    Update PDEsolver.h

--- Project 5/PDEsolver.h
+++ Project 5/PDEsolver.h
@@ -15,11 +15,11 @@ using namespace arma;
 void writingfunc1D(int, vec, ostream&);
 void writingfunc2D(int,mat, ostream&);
 void explicitsch1D(int,double, int);
-void explicitsch2D(int,double, int);
+mat explicitsch2D(int,double, int);
 double *implicit(int,double, double);
 void solver_Thomas(double*, double *, double *, double *, double *, int);
 void CN(int , double , int , ostream&);
-int implicit2D(int, double, int);
-int jacobi(int, double, mat &, double, int);
+mat implicit2D(int, double, int);
+mat jacobi(int, double, mat &, double, int);
 
 #endif
